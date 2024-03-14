114569990b6f4ee2bfa15e96abea59d64897d145
Fairooz2150 <fairooz2150@gmail.com>
2024-03-14 17:01:23 +0530

    New Project

---
+++ assets/js/contact.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+   
+        
+ })(jQuery)
+})
